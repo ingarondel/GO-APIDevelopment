51c1e722aee6294e58e1a0ef2065882e98627972
ingarondel <inga404@icloud.com>
2024-12-25 19:51:28 +0300

    feat: cart_item repository with create and delete functions

---
+++ internal/repository/cart_item.go
@@ -0,0 +1,25 @@
+package repository
+
+import (
+    "github.com/jmoiron/sqlx"
+    "github.com/ingarondel/GO-APIDevelopment/model" 
+)
+
+type CartItemRepository struct {
+    db *sqlx.DB
+}
+
+func NewCartItemRepository(db *sqlx.DB) *CartItemRepository {
+    return &CartItemRepository{db}
+}
+
+func (r *CartItemRepository) CreateCartItem(item *model.CartItem) error {
+    query := "INSERT INTO cart_items (cart_id, product, quantity) VALUES ($1, $2, $3) RETURNING id"
+    return r.db.QueryRow(query, item.CartID, item.Produc, item.Quantity).Scan(&item.ID)
+}
+
+func (r *CartItemRepository) DeleteCartItem(id int64) error {
+    query := "DELETE FROM cart_items WHERE id = $1"
+    _, err := r.db.Exec(query, id)
+    return err
+}
