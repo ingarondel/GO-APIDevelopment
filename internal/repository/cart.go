d363a27bd0ee4a48bdded564e24702ce7bc3a7e5
ingarondel <inga404@icloud.com>
2024-12-25 16:07:12 +0300

    feat: getcart func

--- internal/repository/cart.go
+++ internal/repository/cart.go
@@ -13,7 +13,14 @@ func NewCartRepository(db *sqlx.DB) *CartRepository {
     return &CartRepository{db}
 }
 
-func (r *CartRepository) CreateCart(cart *models.Cart) error {
+func (r *CartRepository) CreateCart(cart *model.Cart) error {
     query := "INSERT INTO carts DEFAULT VALUES RETURNING id"
     return r.db.QueryRowx(query).Scan(&cart.ID)
+}
+
+func (r *CartRepository) GetCart(id int64) (model.Cart, error) {
+    query := "SELECT id FROM carts WHERE id = $1"
+    var cart models.Cart
+    err := r.db.Get(&cart, query, id)
+    return cart, err
 }
