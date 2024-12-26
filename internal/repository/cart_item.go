1b3b0ba9b5ef0950ded74775f4198285ebb5d1ec
ingarondel <inga404@icloud.com>
2024-12-26 14:10:06 +0300

    fix: DeleteCartItem func ID's, add GetCartItems func

--- internal/repository/cart_item.go
+++ internal/repository/cart_item.go
@@ -2,7 +2,7 @@ package repository
 
 import (
     "github.com/jmoiron/sqlx"
-    "github.com/ingarondel/GO-APIDevelopment/internal/model" 
+    "github.com/ingarondel/GO-APIDevelopment/internal/model"
 )
 
 type CartItemRepository struct {
@@ -13,13 +13,20 @@ func NewCartItemRepository(db *sqlx.DB) *CartItemRepository {
     return &CartItemRepository{db}
 }
 
+func (r *CartItemRepository) GetCartItems(cartID int64) ([]model.CartItem, error) {
+    query := "SELECT id, cart_id, product, quantity FROM cart_items WHERE cart_id = $1"
+    var items []model.CartItem
+    err := r.db.Select(&items, query, cartID)
+    return items, err
+}
+
 func (r *CartItemRepository) CreateCartItem(item *model.CartItem) error {
     query := "INSERT INTO cart_items (cart_id, product, quantity) VALUES ($1, $2, $3) RETURNING id"
     return r.db.QueryRow(query, item.CartID, item.Product, item.Quantity).Scan(&item.ID)
 }
 
-func (r *CartItemRepository) DeleteCartItem(id int64) error {
-    query := "DELETE FROM cart_items WHERE id = $1"
-    _, err := r.db.Exec(query, id)
+func (r *CartItemRepository) DeleteCartItem(cartID, itemID int64) error {
+    query := "DELETE FROM cart_items WHERE id = $1 AND cart_id = $2"
+    _, err := r.db.Exec(query, itemID, cartID)
     return err
 }
