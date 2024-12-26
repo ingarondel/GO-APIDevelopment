ea5c784cad6754d8cfb40d3eb8864a31a0a0162c
ingarondel <inga404@icloud.com>
2024-12-26 14:24:26 +0300

    feat: cart_item handler

---
+++ internal/handler/cart_item.go
@@ -0,0 +1,62 @@
+package handler
+
+import (
+    "encoding/json"
+    "net/http"
+    "strconv"
+
+    "github.com/gorilla/mux"
+    "github.com/ingarondel/GO-APIDevelopment/internal/model"
+    "github.com/ingarondel/GO-APIDevelopment/internal/repository"
+)
+
+type CartItemHandler struct {
+    cartItemRepo *repository.CartItemRepository
+}
+
+func NewCartItemHandler(cartItemRepo *repository.CartItemRepository) *CartItemHandler {
+    return &CartItemHandler{cartItemRepo}
+}
+
+func (h *CartItemHandler) AddCartItem(w http.ResponseWriter, r *http.Request) {
+    vars := mux.Vars(r)
+    cartID, _ := strconv.ParseInt(vars["cartId"], 10, 64)
+    var item model.CartItem
+    if err := json.NewDecoder(r.Body).Decode(&item); err != nil || item.Product == "" || item.Quantity <= 0 {
+        http.Error(w, "Invalid input", http.StatusBadRequest)
+        return
+    }
+    item.CartID = cartID
+    if err := h.cartItemRepo.CreateCartItem(&item); err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+    w.WriteHeader(http.StatusCreated)
+    json.NewEncoder(w).Encode(item)
+}
+
+func (h *CartItemHandler) DeleteCartItem(w http.ResponseWriter, r *http.Request) {
+    vars := mux.Vars(r)
+    cartID, _ := strconv.ParseInt(vars["cartId"], 10, 64)
+    itemID, _ := strconv.ParseInt(vars["itemId"], 10, 64)
+
+    if err := h.cartItemRepo.DeleteCartItem(cartID, itemID); err != nil {
+        http.Error(w, err.Error(), http.StatusNotFound)
+        return
+    }
+    w.WriteHeader(http.StatusNoContent)
+}
+
+func (h *CartItemHandler) GetCartItems(w http.ResponseWriter, r *http.Request) {
+    vars := mux.Vars(r)
+    cartID, _ := strconv.ParseInt(vars["cartId"], 10, 64)
+
+    items, err := h.cartItemRepo.GetCartItems(cartID)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+
+    w.WriteHeader(http.StatusOK)
+    json.NewEncoder(w).Encode(items)
+}
