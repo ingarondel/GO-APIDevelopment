b2376d28f6aa037775c3f57343d5906237060d49
ingarondel <inga404@icloud.com>
2025-01-05 14:27:54 +0300

    refactor: use sqldb & context

--- internal/repository/cart_item.go
+++ internal/repository/cart_item.go
@@ -1,32 +1,41 @@
 package repository
 
 import (
-    "github.com/jmoiron/sqlx"
+    "context"
+    "database/sql"
+    "time"
+
     "github.com/ingarondel/GO-APIDevelopment/internal/model"
 )
 
 type CartItemRepository struct {
-    db *sqlx.DB
+    db *sql.DB
 }
 
-func NewCartItemRepository(db *sqlx.DB) *CartItemRepository {
+func NewCartItemRepository(db *sql.DB) *CartItemRepository {
     return &CartItemRepository{db}
 }
 
-func (r *CartItemRepository) GetCartItems(cartID int64) ([]model.CartItem, error) {
+func (r *CartItemRepository) GetCartItems(ctx context.Context, cartID int64) ([]model.CartItem, error) {
+    ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+    defer cancel()
     query := "SELECT id, cart_id, product, quantity FROM cart_items WHERE cart_id = $1"
     var items []model.CartItem
-    err := r.db.Select(&items, query, cartID)
+    err := r.db.Select(&items, ctx, query, cartID)
     return items, err
 }
 
-func (r *CartItemRepository) CreateCartItem(item *model.CartItem) error {
+func (r *CartItemRepository) CreateCartItem(ctx context.Context, item *model.CartItem) error {
+    ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+    defer cancel()
     query := "INSERT INTO cart_items (cart_id, product, quantity) VALUES ($1, $2, $3) RETURNING id"
-    return r.db.QueryRow(query, item.CartID, item.Product, item.Quantity).Scan(&item.ID)
+    return r.db.QueryRowContext(ctx, query, item.CartID, item.Product, item.Quantity).Scan(&item.ID)
 }
 
-func (r *CartItemRepository) DeleteCartItem(cartID, itemID int64) error {
+func (r *CartItemRepository) DeleteCartItem(ctx context.Context, cartID, itemID int64) error {
+    ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+    defer cancel()
     query := "DELETE FROM cart_items WHERE id = $1 AND cart_id = $2"
-    _, err := r.db.Exec(query, itemID, cartID)
+    _, err := r.db.Exec(ctx, query, itemID, cartID)
     return err
 }
