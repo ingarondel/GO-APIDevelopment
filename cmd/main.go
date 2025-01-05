897082bbc6a68b4d10e4264b35ddb8e60b9681ee
ingarondel <inga404@icloud.com>
2025-01-05 15:37:32 +0300

    fix: handler

--- cmd/main.go
+++ cmd/main.go
@@ -6,7 +6,7 @@ import (
 
     "github.com/gorilla/mux"
     "github.com/ingarondel/GO-APIDevelopment/internal/repository"
-    "github.com/ingarondel/GO-APIDevelopment/internal/route"
+    "github.com/ingarondel/GO-APIDevelopment/internal/handler"
 )
 
 func main() {
@@ -17,7 +17,7 @@ func main() {
     defer db.Close()
 
     r := mux.NewRouter()
-    route.Routes(r, db)
+    handler.Routes(r, db)
 
     log.Println("Server started on :3000")
     if err := http.ListenAndServe(":3000", r); err != nil {
