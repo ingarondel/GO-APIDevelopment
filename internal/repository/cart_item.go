8d4b3d53a353feeb44857d77900b5d7d80ca85e1
ingarondel <inga404@icloud.com>
2025-01-05 15:14:10 +0300

    fix

--- internal/repository/cart_item.go
+++ internal/repository/cart_item.go
@@ -20,9 +20,22 @@ func (r *CartItemRepository) GetCartItems(ctx context.Context, cartID int64) ([]
     ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
     defer cancel()
     query := "SELECT id, cart_id, product, quantity FROM cart_items WHERE cart_id = $1"
+    rows, err := r.db.QueryContext(ctx, query, cartID)
+    if err != nil {
+        return nil, err
+    }
+    defer rows.Close()
     var items []model.CartItem
-    err := r.db.Select(&items, ctx, query, cartID)
-    return items, err
+
+    for rows.Next() {
+      var item model.CartItem
+      if err := rows.Scan(&item.ID, &item.CartID, &item.Product, &item.Quantity); err != nil {
+        return nil, err
+      }
+      items = append(items, item)
+    }
+    
+    return items, rows.Err()
 }
 
 func (r *CartItemRepository) CreateCartItem(ctx context.Context, item *model.CartItem) error {
@@ -36,6 +49,7 @@ func (r *CartItemRepository) DeleteCartItem(ctx context.Context, cartID, itemID 
     ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
     defer cancel()
     query := "DELETE FROM cart_items WHERE id = $1 AND cart_id = $2"
-    _, err := r.db.Exec(ctx, query, itemID, cartID)
+    _, err := r.db.ExecContext(ctx, query, itemID, cartID)
+
     return err
 }
