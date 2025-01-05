f9d90bb6a054af6908ca584d907c9813011dfe06
ingarondel <inga404@icloud.com>
2025-01-05 15:12:38 +0300

    fix

--- cmd/main.go
+++ cmd/main.go
@@ -5,18 +5,19 @@ import (
     "net/http"
 
     "github.com/gorilla/mux"
-    "github.com/ingarondel/GO-APIDevelopment/internal/db"
+    "github.com/ingarondel/GO-APIDevelopment/internal/repository"
     "github.com/ingarondel/GO-APIDevelopment/internal/route"
 )
 
 func main() {
-    if err := db.Connect(); err != nil {
+    db, err := repository.Connect()
+    if err != nil {
         log.Fatal("Database connection failed:", err)
     }
+    defer db.Close()
 
     r := mux.NewRouter()
-    
-    route.Routes(r)
+    route.Routes(r, db)
 
     log.Println("Server started on :3000")
     if err := http.ListenAndServe(":3000", r); err != nil {
