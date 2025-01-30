a7540fe48862d978d3b94811d5b6786907df0d27
ingarondel <inga404@icloud.com>
2025-01-31 00:10:02 +0300

    doing main empty

--- internal/model/cart.go
+++
@@ -1,5 +0,0 @@
-package model
-
-type Cart struct {
-	ID int64 `db:"id"`
-}
--- internal/model/cart_item.go
+++
@@ -1,8 +0,0 @@
-package model
-
-type CartItem struct {
-    ID       int64  `db:"id"`
-    CartID   int64  `db:"cart_id"`
-    Product  string `db:"product"`
-    Quantity int    `db:"quantity"`
-}
