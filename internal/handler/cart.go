30670cd1cbb15f7834549c40fd484f3c18efc2f9
PainIsWhenSomeBodyDropT3 <145275963+PainIsWhenSomeBodyDropT3@users.noreply.github.com>
2025-01-31 00:31:48 +0300

    Dev to main (#14)
    * feat: cart handler
    
    * feat: cart_item handler
    
    * feat: create dotenv file and add it to gitignore
    
    * feat: db connection
    
    * feat: routes
    
    * fix: go.mod
    
    * feat
    
    * refactor: move from db to repository
    
    * fix
    
    * fix
    
    * fix: sqlx to sqldb
    
    * fix: handler
    
    * fix: replace router to handler
    
    ---------
    
    Co-authored-by: ingarondel <inga404@icloud.com>

---
+++ internal/handler/cart.go
@@ -0,0 +1,60 @@
+package handler
+
+import (
+    "encoding/json"
+    "net/http"
+    "strconv"
+
+    "github.com/gorilla/mux"
+    "github.com/ingarondel/GO-APIDevelopment/internal/model"
+    "github.com/ingarondel/GO-APIDevelopment/internal/repository"
+)
+
+type CartHandler struct {
+    cartRepo     *repository.CartRepository
+    cartItemRepo *repository.CartItemRepository
+}
+
+func NewCartHandler(cartRepo *repository.CartRepository, cartItemRepo *repository.CartItemRepository) *CartHandler {
+    return &CartHandler{cartRepo, cartItemRepo}
+}
+
+func (h *CartHandler) CreateCart(w http.ResponseWriter, r *http.Request) {
+    cart := &model.Cart{}
+
+    ctx := r.Context()  
+    if err := h.cartRepo.CreateCart(ctx, cart); err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+    w.WriteHeader(http.StatusCreated)
+    json.NewEncoder(w).Encode(cart)
+}
+
+func (h *CartHandler) GetCart(w http.ResponseWriter, r *http.Request) {
+    vars := mux.Vars(r)
+    cartID, _ := strconv.ParseInt(vars["cartId"], 10, 64)
+
+    ctx := r.Context()
+
+    cart, err := h.cartRepo.GetCart(ctx, cartID)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusNotFound)
+        return
+    }
+
+
+    items, err := h.cartItemRepo.GetCartItems(ctx, cartID)
+
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+
+    response := map[string]interface{}{
+        "id":    cart.ID,
+        "items": items,
+    }
+    w.WriteHeader(http.StatusOK)
+    json.NewEncoder(w).Encode(response)
+}
