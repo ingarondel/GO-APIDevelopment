06137fdd3e7cd2c3f232205b2eb7c119ced4a1c8
ingarondel <inga404@icloud.com>
2025-02-01 00:05:16 +0300

    fix: cart repository

--- internal/repository/cart.go
+++ internal/repository/cart.go
@@ -15,39 +15,29 @@ type CartRepository struct {
 func NewCartRepository(db *sql.DB) *CartRepository {
     return &CartRepository{db}
 }
-// TODO context.WithTimeout(ctx, 5*time.Second) - это делается на уровне handler тк именно там ты задаешь время на выполнение запроса в целом
-// здесь лишь вызовы к бд
-// TODO тебе нужно возвращать (model.Cart , error)
-func (r *CartRepository) CreateCart(ctx context.Context, cart *model.Cart) error {
-    ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
-    defer cancel()
-    query := "INSERT INTO carts DEFAULT VALUES RETURNING id"
-    // TODO нужно обязатель оборачивать ошибки 
-    // if err!=nil{
-    //     ...
-    // }
 
-    return r.db.QueryRowContext(ctx, query).Scan(&cart.ID)
+func (r *CartRepository) CreateCart(ctx context.Context, cart *model.Cart) error {
+    query := "INSERT INTO carts DEFAULT VALUES RETURNING id"
+
+    err := r.db.QueryRowContext(ctx, query).Scan(&cart.ID)
+      if err!=nil {
+        return fmt.Errorf("failed to create cart: %w", err)
+      }
+    return nil
 }
-// TODO context.WithTimeout(ctx, 5*time.Second) - это делается на уровне handler тк именно там ты задаешь время на выполнение запроса в целом
-// здесь лишь вызовы к бд
+
 func (r *CartRepository) GetCart(ctx context.Context, id int64) (model.Cart, error) {
-    ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
-    defer cancel()
     query := "SELECT id FROM carts WHERE id = $1"
     var cart model.Cart
-        // TODO а что если такой cart не существует в базе?
-        // в этом случае тебе нужно вернуть ошибку о том, что cart не существует 
-        // рекомендую создать свой тип ошибки
 
-    // TODO нужно обязатель оборачивать ошибки 
-    // if err!=nil{
-    //     ...
-    // }
     err := r.db.QueryRowContext(ctx, query, id).Scan(&cart.ID)
-    return cart, err
+    if err != nil {
+        if errors.Is(err, sql.ErrNoRows){
+            return cart, errors.New("cart not found")
+        }
+    }
+      if err!=nil{
+        return fmt.Errorf("failed to get cart: %w", err)
+      }    
+    return cart, nil
 }
-
-//во все методы репозитория нужно передавать контекстс
-//внутри каждого метода нужно создавать новый контекст с таймаутом допустим на 5 сек
-//использовать sqldb
